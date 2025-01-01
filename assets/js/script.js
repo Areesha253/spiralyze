c5021c3ab65b2a55019d552d426c4a0817a73e39
Areesha Hanif <areeshahanif253@gmail.com>
2025-01-01 17:04:01 +0500

    update style and jquery for hero-form

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,40 +19,42 @@ $("#contact-form").on("submit", function (e) {
   const company = $("#user-company").val();
 
   let formValid = true;
-  if (!firstName) {
-    formValid = false;
-    $("#user-first-name").siblings(".empty-field-warning").show();
-    $("#user-first-name").closest(".input-field").addClass("error");
-  } else if (!lastName) {
-    formValid = false;
-    $("#user-last-name").siblings(".empty-field-warning").show();
-    $("#user-last-name").closest(".input-field").addClass("error");
-  } else if (!businessEmail) {
-    formValid = false;
-    $("#user-business-email").siblings(".empty-field-warning").show();
-    $("#user-business-email").closest(".input-field").addClass("error");
-  } else if (!company) {
-    formValid = false;
-    $("#user-company").siblings(".empty-field-warning").show();
-    $("#user-company").closest(".input-field").addClass("error");
-  }
+  const fields = [
+    {
+      id: "#user-first-name",
+      value: firstName,
+    },
+    {
+      id: "#user-last-name",
+      value: lastName,
+    },
+    {
+      id: "#user-business-email",
+      value: businessEmail,
+    },
+    {
+      id: "#user-company",
+      value: company,
+    },
+  ];
 
+  for (const field of fields) {
+    if (!field.value) {
+      formValid = false;
+      $(field.id).siblings(".empty-field-warning").show();
+      $(field.id).closest(".input-field").addClass("error");
+      break;
+    }
+  }
   if (formValid) {
     console.log(firstName);
     console.log(lastName);
     console.log(businessEmail);
     console.log(company);
-
-    $("#user-first-name").val("");
-    $("#user-last-name").val("");
-    $("#user-business-email").val("");
-    $("#user-company").val("");
+    $("input").val("");
   }
   $("input").on("input", function () {
     $(this).siblings(".empty-field-warning").hide();
-    if ($(this).val()) {
-      $(this).closest(".input-field").removeClass("error");
-    }
-    $(this).val();
+    $(this).val() && $(this).closest(".input-field").removeClass("error");
   });
 });
